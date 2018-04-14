1feb5d733dbe693ffefb3516dab2b3c4dbb5e350
glowing-chemist <olie.smit0@gmail.com>
2018-04-14 20:54:11 +0100

    Add the header file for the PipelineManager.

--- Src/ThiefVKPipeLineManager.hpp
+++ Src/ThiefVKPipeLineManager.hpp
@@ -1,5 +1,39 @@
 #ifndef THIEFVKPIPELINEMANAGER_HPP
 #define THIEFVKPIPELINEMANAGER_HPP
 
+#include <map> // used for a runtime pipeline cache
+
+#include <vulkan/vulkan.hpp>
+
+enum class ShaderName {
+    BasicTransformVertex,
+    BasicColourFragment,
+    DepthVertex,
+    DepthFragment,
+    LightVertex,
+    LightFragment,
+    NormalVertex,
+    NormalFragment
+};
+
+struct ThiefVKPipelineDescription {
+    ShaderName vertexShader;
+    ShaderName fragmentShader;
+
+    int renderTargetWidth; // -1 represents sizeof swap chain
+    int renderTargetHeight;
+    int renderTargetOffset;
+};
+
+class ThiefVKPipelineManager {
+public:
+    ThiefVKPipelineManager();
+    void Destroy(vk::Device);
+
+    vk::Pipeline getPipeLine(ThiefVKPipelineDescription);
+private:
+
+    std::map<ThiefVKPipelineDescription, vk::Pipeline> pipeLineCache;
+};
 
 #endif
