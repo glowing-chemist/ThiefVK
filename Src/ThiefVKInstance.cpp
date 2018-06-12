cafbbe1bc631b5af08a93b25d807d9a72c8c3539
oliver smith <olie.smit0@gmail.com>
2018-06-12 21:35:25 +0200

    Use the built in __debugbreak on windows when we hit a vulkan validation error.

--- Src/ThiefVKInstance.cpp
+++ Src/ThiefVKInstance.cpp
@@ -25,7 +25,11 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallbackFunc(
 
     std::cerr << "validation layer: " << msg << std::endl;
 
+#ifdef _MSC_VER 
+	__debugbreak;
+#else
     asm("int3");
+#endif
 
     return VK_FALSE;
 
