09ff98bbbd9c3b5b420be3806e218c3ce0bc8707
Oliver Smith <olie.smit0@gmail.com>
2018-07-23 09:45:50 +0100

    Add this test draw as I currently have bo validation errors but still nothing gets drawn. testing on intel next.

--- Src/ThiefVKEngine.cpp
+++ Src/ThiefVKEngine.cpp
@@ -3,6 +3,7 @@
 #include "ThiefVKDevice.hpp"
 #include "ThiefVKInstance.hpp"
 
+#include <glm/gtc/matrix_transform.hpp>
 
 ThiefVKEngine::ThiefVKEngine() : Instance{ThiefVKInstance()},
                                  Device{ThiefVKDevice(Instance.findSuitableDevices(
@@ -26,6 +27,21 @@ void ThiefVKEngine::Init() {
 
   for(int i = 0; i < 100; i++) {
 	     Device.startFrame();
+
+       geometry geom{std::vector<Vertex>{    Vertex{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+                          Vertex{{0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+                          Vertex{{0.5f, 0.5f, 0.0f},  {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+                          Vertex{{-0.5f, 0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+                          Vertex{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+                          Vertex{{0.5f, 0.5f, 0.0f},  {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},},
+                          glm::rotate(glm::mat4(1.0f), i * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, -1.0f)),
+                          glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
+                          glm::perspective(glm::radians(45.0f), 500 / (float) 500, 0.1f, 10.0f),
+                          "./statue.jpg"};
+
+      Device.draw(geom);
+
+
        Device.endFrame();
        Device.swap();
   }
