1eb00b1e3ab66df3d6c72cdf3e4792c593e6723f
Oliver Smith <olie.smit0@gmail.com>
2018-07-26 18:32:59 +0100

    Update the test draw to use the correct camera.

--- Src/ThiefVKEngine.cpp
+++ Src/ThiefVKEngine.cpp
@@ -28,15 +28,21 @@ void ThiefVKEngine::Init() {
   for(int i = 0; i < 100; i++) {
 	     Device.startFrame();
 
+        auto model = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        auto view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        auto proj = glm::perspective(glm::radians(45.0f), 500 / (float) 500, 0.1f, 10.0f);
+
+        //proj[1][1] *= -1; // to map from gl to vulkan space
+
        geometry geom{std::vector<Vertex>{    Vertex{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
                           Vertex{{0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
                           Vertex{{0.5f, 0.5f, 0.0f},  {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-                          Vertex{{-0.5f, 0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
-                          Vertex{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
-                          Vertex{{0.5f, 0.5f, 0.0f},  {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},},
-                          glm::rotate(glm::mat4(1.0f), i * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, -1.0f)),
-                          glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
-                          glm::perspective(glm::radians(45.0f), 500 / (float) 500, 0.1f, 10.0f),
+                          Vertex{{0.5f, 0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+                          Vertex{{-0.5f, 0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+                          Vertex{{-0.5f, -0.5f, 0.0f},  {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},},
+                          model,
+                          view,
+                          proj,
                           "./statue.jpg"};
 
       Device.draw(geom);
