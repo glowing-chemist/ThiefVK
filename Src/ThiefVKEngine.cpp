068a140a5a11c10ce600a64031bd74e3b7ca4afc
Oliver Smith <olie.smit0@gmail.com>
2018-07-30 21:06:29 +0100

    Udate the test scene.

--- Src/ThiefVKEngine.cpp
+++ Src/ThiefVKEngine.cpp
@@ -25,21 +25,21 @@ void ThiefVKEngine::Init() {
 	Device.createCommandPools();
   Device.createSemaphores();
 
-  for(int i = 0; i < 100; i++) {
+  for(int i = 0; i < 300; i++) {
 	     Device.startFrame();
 
-        auto model = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        auto view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        auto model = glm::rotate(glm::mat4(1.0f),  (i / 100.0f) * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        auto view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f));
         auto proj = glm::perspective(glm::radians(45.0f), 500 / (float) 500, 0.1f, 10.0f);
 
         //proj[1][1] *= -1; // to map from gl to vulkan space
 
-       geometry geom{std::vector<Vertex>{    Vertex{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
-                          Vertex{{0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+       geometry geom{std::vector<Vertex>{    Vertex{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+                          Vertex{{0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
                           Vertex{{0.5f, 0.5f, 0.0f},  {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-                          Vertex{{0.5f, 0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+                          Vertex{{0.5f, 0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
                           Vertex{{-0.5f, 0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
-                          Vertex{{-0.5f, -0.5f, 0.0f},  {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},},
+                          Vertex{{-0.5f, -0.5f, 0.0f},  {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},},
                           model,
                           view,
                           proj,
